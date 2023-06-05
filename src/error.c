f131d881e23e3b3e8967edc6ced94c18ca40c837
cwenz <cwenz@student.42heilbronn.de>
2023-06-05 14:13:37 +0200

    added error file to exit and display error

--- src/error.c
+++ src/error.c
@@ -6,13 +6,14 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 18:22:53 by cwenz             #+#    #+#             */
-/*   Updated: 2023/05/28 18:23:13 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/06/05 14:11:54 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void error_message()
+void display_error_and_exit()
 {
-	ft_printf("Error\n");
+	write(2, "Error\n", 6);
+	exit(0);
 }
