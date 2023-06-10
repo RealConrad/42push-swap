c55320bebf4a14287e6ced10fc2467658530e1df
cwenz <wenzconrad@gmail.com>
2023-06-10 14:34:23 +0200

    improved init function and added orginal_pos to numbers

--- src/initialize/initialize.c
+++ src/initialize/initialize.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 13:25:15 by cwenz             #+#    #+#             */
-/*   Updated: 2023/06/06 14:01:20 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/06/10 14:25:51 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,28 @@
 
 void init_stack(t_stack *stack, int argc, char **argv)
 {
-	if (argc == 1) // Handling input parsed as 1 string
-		handle_one_input(stack, argv);
-	else // Handling input parsed as multiple inputs
-		handle_multiple_inputs(stack, argc, argv);
+	char	**split_strings;
+	int		i;
+	int		j;
+	int		value;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = 0;
+		split_strings = ft_split(argv[i], ' ');
+		if (!split_strings)
+			display_error_and_exit();
+		while (split_strings[j])
+		{
+			value = ft_atoi(split_strings[j]);
+			if (!is_number(split_strings[j]) || is_duplicate(stack, value))
+				display_error_and_exit();
+			add_node_to_end(stack, value);
+			free(split_strings[j]);
+			j++;
+		}
+		free (split_strings);
+		i++;
+	}
 }
--- src/initialize/initialize_utils.c
+++ src/initialize/initialize_utils.c
@@ -6,56 +6,13 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:24:09 by cwenz             #+#    #+#             */
-/*   Updated: 2023/06/06 15:49:16 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/06/10 14:33:41 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-static bool	is_number(char *str);
-static void	add_node_to_end(t_stack *stack, int node_value);
-static bool	is_duplicate(t_stack *stack, int value);
-
-void	handle_multiple_inputs(t_stack *stack, int argc, char **argv)
-{
-	int	i;
-	int	value;
-
-	i = 0;
-	while (i < argc)
-	{
-		value = ft_atoi(argv[i]);
-		if (!is_number(argv[i]) || is_duplicate(stack, value))
-			display_error_and_exit();
-		add_node_to_end(stack, value);
-		i++;
-	}
-}
-
-void	handle_one_input(t_stack *stack, char **argv)
-{
-	char	**split_strings;
-	int		i;
-	int		value;
-
-	i = 0;
-	// start at index 0 as we incremented back in the main to skip program name
-	split_strings = ft_split(argv[0], ' ');
-	if (!split_strings || split_strings[0] == NULL)
-		display_error_and_exit();
-	while (split_strings[i])
-	{
-		value = ft_atoi(split_strings[i]);
-		if (!is_number(split_strings[i]) || is_duplicate(stack, value))
-			display_error_and_exit();
-		add_node_to_end(stack, value);
-		free(split_strings[i]);
-		i++;
-	}
-	free(split_strings);
-}
-
-static void	add_node_to_end(t_stack *stack, int node_value)
+void	add_node_to_end(t_stack *stack, int node_value)
 {
 	t_node	*new_node;
 
@@ -65,6 +22,7 @@ static void	add_node_to_end(t_stack *stack, int node_value)
 	new_node->value = node_value;
 	new_node->next = NULL;
 	new_node->prev = stack->tail;
+	new_node->original_pos = stack->size + 1; // use stack size to determine original pos
 	stack->size++;
 	if (!stack->head) // If the stack is not initialized yet
 	{
@@ -78,7 +36,7 @@ static void	add_node_to_end(t_stack *stack, int node_value)
 	}
 }
 
-static bool	is_number(char *str)
+bool	is_number(char *str)
 {
 	int	i;
 
