715771437e62e4649901579d61427be78bb70d59
cwenz <wenzconrad@gmail.com>
2023-06-11 17:08:33 +0200

    fixed bug in swap function

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -6,27 +6,32 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 14:12:24 by cwenz             #+#    #+#             */
-/*   Updated: 2023/06/10 17:13:26 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/06/11 17:07:02 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void	swap(t_stack *stack, t_operation operation)
+void swap(t_stack *stack, t_operation operation)
 {
-	t_node	*old_head;
-	
-	if (stack->size <= 1)
-		return ;
+    t_node *old_head;
 
-	old_head = stack->head;
-	stack->head = old_head->next;
-	
-	old_head->next = stack->head->next;
-	old_head->prev = stack->head;
+    if (stack->size <= 1)
+        return;
 
-	stack->head->prev = NULL;
-	stack->head->next = old_head;
-	
-	print_operation(operation);
+    old_head = stack->head;
+
+    // Change the head of the stack to the second node
+    stack->head = old_head->next;
+    stack->head->prev = NULL; // The new head's 'prev' should be NULL
+
+    // Now update the links in the old head
+    old_head->next = stack->head->next;
+    old_head->prev = stack->head;
+    old_head->next->prev = old_head; // Update third node 'prev' to point to the old head
+
+    // Update the link in the new head
+    stack->head->next = old_head;
+
+    print_operation(operation);
 }
