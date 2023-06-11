ad5c585fcdcc1041163701c42c62516dd08fe155
cwenz <wenzconrad@gmail.com>
2023-06-11 17:09:28 +0200

    removed comments

--- src/main.c
+++ src/main.c
@@ -6,40 +6,11 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:27:19 by cwenz             #+#    #+#             */
-/*   Updated: 2023/06/07 14:10:46 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/06/10 16:59:28 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-/*
- At the beginning:
-◦ The stack a contains a random amount of negative and/or positive numbers
-which cannot be duplicated.
-◦ The stack b is empty.
-• The goal is to sort in ascending order numbers into stack a.
- 
-To do so you have the following operations at your disposal:
-1. sa (swap a): Swap the first 2 elements at the top of stack a.
-	> Do nothing if there is only one or no elements.
-2. sb (swap b): Swap the first 2 elements at the top of stack b.
-	> Do nothing if there is only one or no elements.
-3. ss : sa and sb at the same time.
-4. pa (push a): Take the first element at the top of b and put it at the top of a.
-	> Do nothing if b is empty.
-5. pb (push b): Take the first element at the top of a and put it at the top of b.
-	> Do nothing if a is empty.
-6. ra (rotate a): Shift up all elements of stack a by 1.
-	> The first element becomes the last one.
-7. rb (rotate b): Shift up all elements of stack b by 1.
-	> The first element becomes the last one.
-8. rr : ra and rb at the same time.
-9. rra (reverse rotate a): Shift down all elements of stack a by 1
-	> The last element becomes the first one.
-10. rrb (reverse rotate b): Shift down all elements of stack b by 1.
-	> The last element becomes the first one.
-11. rrr : rra and rrb at the same time.
-*/
 
 int	main(int argc, char **argv)
 {
@@ -56,17 +27,18 @@ int	main(int argc, char **argv)
 	init_stack(&stack_a, --argc, ++argv); // Skip program name
 	start_sorting(&stack_a, &stack_b);
 	
-	// ft_printf("Linked List A: ");
-	// print_list(stack_a.head);
+	print_list(stack_a.head);
 	return (0);
 }
 
 // Function to print the linked list
 void	print_list(t_node *node)
 {
+	ft_printf("Sorted list: ");
 	while (node != NULL)
 	{
 		ft_printf("%d ", node->value);
+		// ft_printf("I: %d :: N: %d\n", node->original_pos, node->value);
 		node = node->next;
 	}
 	ft_printf("\n");
