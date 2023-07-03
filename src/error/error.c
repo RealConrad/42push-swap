ade3cee352506cfda33e0e740db6cc93895625bd
cwenz <wenzconrad@gmail.com>
2023-07-03 17:32:28 +0200

    updated error function

--- src/error/error.c
+++ src/error/error.c
@@ -6,14 +6,23 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 18:22:53 by cwenz             #+#    #+#             */
-/*   Updated: 2023/06/18 18:02:16 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/03 17:29:03 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void display_error_and_exit()
+void free_and_exit(t_stack *stack, t_exit exit_type)
 {
-	write(2, "Error\n", 6);
-	exit(1);
+	if (exit_type == FAIL)
+	{
+		write(2, "Error\n", 6);
+		free_stack(stack);
+		exit(1);
+	}
+	else if (exit_type == SUCCESS)
+	{
+		free_stack(stack);
+		exit(0);
+	}
 }
