c84b9deba05c223e23c16cf4888658a476577f8a
cwenz <wenzconrad@gmail.com>
2023-07-04 18:23:39 +0200

    fixed bug with 2 values given

--- src/sort/start_sorting.c
+++ src/sort/start_sorting.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 15:09:45 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/04 14:40:37 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/04 18:22:54 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	start_sorting(t_stack *stack_a, t_stack *stack_b)
 	if (is_sorted(stack_a) || stack_a->size <= 1)
 		free_and_exit(stack_a, SUCCESS);
 	if (stack_a->size == 2)
-		rotate(stack_a, OPERATION_RA);
+		swap(stack_a, OPERATION_SA);
 	else if (stack_a->size == 3)
 		sort_3(stack_a);
 	else if (stack_a->size == 4)
