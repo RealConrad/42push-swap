f22802b8f3bb03dd453382e5a2b70c93db9a2185
cwenz <wenzconrad@gmail.com>
2023-07-07 18:00:41 +0200

    fixed error where struct variable not initialized

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:27:19 by cwenz             #+#    #+#             */
-/*   Updated: 2023/07/04 14:02:32 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/07/07 16:22:59 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ int	main(int argc, char **argv)
 {
 	t_stack	stack_a;
 	t_stack	stack_b;
+	
 	// Initialize stack with null head and tail
 	stack_a.head = NULL;
 	stack_a.tail = NULL;
+	stack_a.size = 0;
 	stack_b.head = NULL;
 	stack_b.tail = NULL;
+	stack_b.size = 0;
 	if (argc <= 1)
 		return (0);
 	init_stack(&stack_a, --argc, ++argv); // Skip program name
